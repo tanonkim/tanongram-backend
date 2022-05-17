3af2b91585945d0e6a8a4a4110837a4ccc601506
tanonkim <robbins4bos@gmail.com>
2022-05-17 13:23:24 +0900

    add : user_signup

---
+++ users/users.mutations.js
@@ -0,0 +1,44 @@
+import client from "../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        // check if username or email are already on DB
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username: username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username or password is already taken.");
+        }
+        // hase password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        // save and return the user
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
