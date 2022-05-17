68000aae25daf3c1c918cc5b5b9189ffa7380d61
tanonkim <robbins4bos@gmail.com>
2022-05-17 14:06:46 +0900

    add : user_signin

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import client from "../client";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export default {
@@ -39,6 +40,30 @@ export default {
       } catch (error) {
         return error;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find user with args.username
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return { ok: false, error: "User not found" };
+      }
+      // check password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password",
+        };
+      }
+      // both pass => issue token and send it to the user
+      const token = await jwt.sign(
+        { id: user.id, exp: Math.floor(Date.now() / 1000) + 60 * 60 },
+        process.env.SECRET_KEY
+      );
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
