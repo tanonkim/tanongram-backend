a84c9d12d4d756a52d787ab621e8e48e1345df9f
tanonkim <robbins4bos@gmail.com>
2022-05-18 17:02:23 +0900

    add : computed fields + search users

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -13,5 +13,9 @@ export default gql`
     avatar: String
     followers: [User]
     following: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
+    isMe: Boolean!
+    isFollowing: Boolean!
   }
 `;
