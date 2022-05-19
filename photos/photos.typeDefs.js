ed59a379a2bbe6c5125111e00300d48de38ffdee
tanonkim <robbins4bos@gmail.com>
2022-05-19 18:47:39 +0900

    add : seePhotoComments

--- photos/photos.typeDefs.js
+++ photos/photos.typeDefs.js
@@ -7,7 +7,9 @@ export default gql`
     file: String!
     caption: String
     likes: Int!
+    comments: Int!
     hashtags: [Hashtag]
+    isMine: Boolean!
     createdAt: String!
     updatedAt: String!
   }
