a3e16a95f00b35985c2f8caf967c4ba8efb00076
tanonkim <robbins4bos@gmail.com>
2022-05-22 16:00:15 +0900

    add : subscriptions

--- messages/sendMessage/sendMessage.resolvers.js
+++ messages/sendMessage/sendMessage.resolvers.js
@@ -1,5 +1,7 @@
 import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
+import pubsub from "../../pubsub";
+import { NEW_MESSAGE } from "../../constants";
 
 export default {
   Mutation: {
@@ -51,7 +53,7 @@ export default {
             };
           }
         }
-        await client.message.create({
+        const message = await client.message.create({
           data: {
             payload,
             room: {
@@ -66,6 +68,7 @@ export default {
             },
           },
         });
+        pubsub.publish(NEW_MESSAGE, { roomUpdates: { ...message } });
         return {
           ok: true,
         };
--- server.js
+++ server.js
@@ -1,41 +1,83 @@
 require("dotenv").config();
 import express from "express";
+import http from "http";
 import logger from "morgan";
+
 import { ApolloServer } from "apollo-server-express";
+import { graphqlUploadExpress } from "graphql-upload";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { execute, subscribe } from "graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
-import { graphqlUploadExpress } from "graphql-upload";
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  context: async ({ req }) => {
-    return { loggedInUser: await getUser(req.headers.token) };
-  },
-});
-const PORT = process.env.PORT;
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
+async function startServer() {
+  const apollo = new ApolloServer({
+    schema,
+    context: async (ctx) => {
+      if (ctx.req) {
+        return {
+          loggedInUser: await getUser(ctx.req.headers.token),
+        };
+      } else {
+        const {
+          connection: { context },
+        } = ctx;
+        return {
+          loggedInUser: context.loggedInUser,
+        };
+      }
     },
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
   });
 
-  await server.start();
-
+  await apollo.start();
   const app = express();
-
-  app.use(graphqlUploadExpress());
   app.use(logger("tiny"));
-  server.applyMiddleware({ app });
   app.use("/static", express.static("uploads"));
+  app.use(graphqlUploadExpress());
+  apollo.applyMiddleware({ app });
 
-  await new Promise((func) => app.listen({ port: PORT }, func));
-  console.log(`🚀 Server: http://localhost:${PORT}${server.graphqlPath}`);
-};
+  const httpServer = http.createServer(app);
+
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+      async onConnect(connectionParams, webSocket, context) {
+        console.log("onConnect!");
+        const { token } = connectionParams;
+        if (!token) {
+          throw new Error("토큰이 존재하지 않습니다.");
+        }
+        const loggedInUser = await getUser(token);
+        return { loggedInUser };
+      },
+      onDisconnect(webSocket, context) {
+        console.log("onDisconnect!");
+      },
+    },
+    { server: httpServer, path: "/graphql" }
+  );
+
+  const PORT = process.env.PORT;
+  await new Promise((resolve) => httpServer.listen(PORT, resolve));
+  console.log(
+    `🚀🚀🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`
+  );
+}
 startServer();
